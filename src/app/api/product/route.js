bdc2838b6956047bec07edc5d15994b2a9ce3c25
YeisonMC <yeisonmacalupu01@gmail.com>
2024-02-16 21:27:15 -0500

    Se puso el force-dynamic por que no se ven los datos

--- src/app/api/product/route.js
+++ src/app/api/product/route.js
@@ -1,6 +1,8 @@
 import prisma from "@/libs/prisma";
 import { NextResponse } from "next/server";
 
+export const dynamic = "force-dynamic";
+
 export async function GET() {
   const Product = await prisma.Product.findMany();
   // const products = await prisma.$queryRaw`SELECT * FROM Product`;
